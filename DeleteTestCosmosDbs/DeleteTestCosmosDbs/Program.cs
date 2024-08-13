80e0086f9ee1e107a5a129421da668fa43d1a534
Mark Williams <mark.williams@enable.com>
2024-08-13 08:57:42 +0100

    Add project files.

---
+++ DeleteTestCosmosDbs/DeleteTestCosmosDbs/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Azure.Cosmos;
+
+using var client = new CosmosClient("AccountEndpoint=https://localhost:8081/;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==");
+
+using var iterator = client.GetDatabaseQueryIterator<DatabaseProperties>();
+
+while (iterator.HasMoreResults)
+{
+    foreach (DatabaseProperties dbProps in await iterator.ReadNextAsync())
+    {
+        if (dbProps.Id.Contains("test", StringComparison.OrdinalIgnoreCase))
+        {
+            Console.WriteLine($"Deleting '{dbProps.Id}'");
+            await client.GetDatabase(dbProps.Id).DeleteAsync();
+        }
+    }
+}
